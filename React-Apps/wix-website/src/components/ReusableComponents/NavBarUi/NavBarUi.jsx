22add65241b23e56d4234ceaa302e3ff321438af
Aakashhm293 <167880779+Aakashhm293@users.noreply.github.com>
2024-05-03 09:54:03 +0530

    Fixed the navigation bar responsiveness

--- React-Apps/wix-website/src/components/ReusableComponents/NavBarUi/NavBarUi.jsx
+++ React-Apps/wix-website/src/components/ReusableComponents/NavBarUi/NavBarUi.jsx
@@ -1,74 +1,74 @@
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import NavDropdown from "react-bootstrap/NavDropdown";
 import { Link } from "react-router-dom";
-import "./NavBarUi.css";
 
-function NavBarUi() {
+export default function NavBarUiBeta() {
   return (
-    <>
-      <nav
-        className="navbar navbar-expand-lg bg-body-tertiary text-center bg-white sticky-top"
-        id="nav-custom"
-      >
-        <div className="container-fluid">
-          <Link className="navbar-brand col-4 my-4" href="#">
-            J.ALISSA
-          </Link>
-          <button
-            className="navbar-toggler"
-            type="button"
-            data-bs-toggle="collapse"
-            data-bs-target="#navbarNav"
-            aria-controls="navbarNav"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
-          >
-            <span className="navbar-toggler-icon"></span>
-          </button>
-          <div
-            className="collapse navbar-collapse col-10 d-flex justify-content-center"
-            id="navbarNav"
-          >
-            <ul className="navbar-nav">
-              <li className="nav-item">
-                <Link className="nav-link active" aria-current="page" to="/">
-                  Home
-                </Link>
-              </li>
-              <li className="nav-item">
-                <Link className="nav-link" to="/portfolio">
-                  Portfolio
-                </Link>
-              </li>
-              <li className="nav-item">
-                <Link className="nav-link" to="/about">
-                  About
-                </Link>
-              </li>
-              <li className="nav-item">
-                <Link className="nav-link" to="/books">
-                  Books
-                </Link>
-              </li>
-              <li className="nav-item">
-                <Link className="nav-link" to="/blogpage">
-                  Blog
-                </Link>
-              </li>
-              <li className="nav-item">
-                <Link className="nav-link" to="/client-albums">
-                  Client-Albums
-                </Link>
-              </li>
-              <li className="nav-item">
-                <Link className="nav-link" to="/contact">
-                  Contact
-                </Link>
-              </li>
-            </ul>
-          </div>
-        </div>
-      </nav>
-    </>
+    <Navbar
+      expand="lg"
+      className="bg-body-tertiary text-center bg-white sticky-top"
+    >
+      <Container>
+        <Navbar.Brand href="#home" className="navbar-brand col-4 my-4">
+          J.ALISSA
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto collapse navbar-collapse col-10 d-flex justify-content-center text-dark">
+            <Nav.Link>
+              <Link className="text-decoration-none text-dark" to="/">
+                Home
+              </Link>
+            </Nav.Link>
+            <Nav.Link>
+              <Link className="text-decoration-none text-dark" to="/portfolio">
+                Portfolio
+              </Link>
+            </Nav.Link>
+            <Nav.Link>
+              <Link className="text-decoration-none text-dark" to="/about">
+                About
+              </Link>
+            </Nav.Link>
+            <Nav.Link>
+              <Link className="text-decoration-none text-dark" to="/books">
+                Books
+              </Link>
+            </Nav.Link>
+            <Nav.Link>
+              <Link className="text-decoration-none text-dark" to="/blogpage">
+                Blog
+              </Link>
+            </Nav.Link>
+            <Nav.Link>
+              <Link
+                className="text-decoration-none text-dark"
+                to="client-albums"
+              >
+                Client-Albums
+              </Link>
+            </Nav.Link>
+            <Nav.Link>
+              <Link className="text-decoration-none text-dark" to="/contact">
+                Contact
+              </Link>
+            </Nav.Link>
+            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">
+                Another action
+              </NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="#action/3.4">
+                Separated link
+              </NavDropdown.Item>
+            </NavDropdown>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
   );
 }
-
-export default NavBarUi;
